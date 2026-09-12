586bf9fdc6f0e3a4ca88f658e657251781cd1179
Winda pratiwi <windaprawinda62@gmail.com>
2026-09-12 22:32:39 +0800

    menambahkan rote / admin

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('front-office.beranda');
 });
+
+Route::get('/admin', function () {
+    return view('back-office.dasbord');
+});
